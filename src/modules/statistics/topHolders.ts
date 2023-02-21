39575c98234f29c7b65723b83a781f48a15882c9
Minh Pham <dev.1foxglobal@gmail.com>
2023-02-21 10:27:39 +0700

    fix: log top holders cron

--- src/modules/statistics/topHolders.ts
+++ src/modules/statistics/topHolders.ts
@@ -1,5 +1,5 @@
 import { CronQueue } from '@/configs/queue';
-import { groupBy, sortArray, sumArrayByField } from '@/core/utils';
+import { groupBy, sortArray, stringifyObjectMsg, sumArrayByField } from '@/core/utils';
 import schedule from 'node-schedule';
 import { getCoinList, getTopHolders } from '../debank/services';
 import { getPortfoliosByWalletAddress } from '../portfolios/services/getPortfolios';
@@ -10,6 +10,8 @@ import { CRON_TASK, Holder, Output, SegmentOptions, SegmentResult } from './type
 import { percentage } from './utils';
 import cronLog from '@/modules/cron_logs';
 import { CronLog } from '../cron_logs/types';
+import Container from 'typedi';
+import { telegramBotToken } from '@/configs/telegram';
 
 const getHotWallets = async (holders) => {
   const addresses = holders
@@ -278,19 +280,39 @@ const saveLogs = async ({ queue, raw_count, crawl_id, job_count }) => {
  * 3. Process data foreach segment/group
  */
 const triggerCronJobs = async (forced_crawl_id?) => {
+  const telegramBot = Container.get(telegramBotToken);
   const queueName = 'top-holders';
   const symbols = await getCoinList();
-  const crawlIds = await getCrawlIds();
+  
+  const crawlIdOptions = await getCrawlIds();
+  let crawlIds = crawlIdOptions.slice(0, 1);
+  if (forced_crawl_id) {
+    crawlIds = crawlIdOptions.filter(({ current }) => current === +forced_crawl_id);
+  } 
 
-  const { addJobs } = await CronQueue({
+  const { queue, addJobs } = await CronQueue({
     name: queueName,
     job_handler: async ({ data }) => {
       return await jobHandler(data);
     },
     drained_callback: async () => {
-      console.log(
-        '🚀 ~ file: index.ts:19 ~ drained_callback: ~ drained_callback',
-      );
+      setTimeout(async () => {
+        const { jobCounts, resultCount } = await saveLogs({
+          queue,
+          crawl_id: crawlIds.map(({ current }) => current).join(','),
+          raw_count: null,
+          job_count: jobs.length
+        });
+
+        const msg = `${queue.name}: queue drained ${stringifyObjectMsg({
+          num_of_jobs: jobs.length,
+          job_queue_status: jobCounts,
+          pg_raw: null,
+          mongo_updated: resultCount,
+        })}`;
+        telegramBot.sendMessage(msg);
+        console.log(msg);
+      }, 60000);
     },
     job_options: {
       // The total number of attempts to try the job until it completes
@@ -306,12 +328,7 @@ const triggerCronJobs = async (forced_crawl_id?) => {
 
   const jobs = [];
   symbols.forEach((symbol) => {
-    let cids = crawlIds.slice(0, 1)
-    if (forced_crawl_id) {
-      cids = crawlIds.filter(({ current }) => current === +forced_crawl_id);
-    } 
-
-    cids.forEach((cid) => {
+    crawlIds.forEach((cid) => {
       SegmentOptions.forEach((segment) => {
         jobs.push({
           symbol,
@@ -328,6 +345,22 @@ const triggerCronJobs = async (forced_crawl_id?) => {
   });
 
   await addJobs(jobs);
+
+  const { jobCounts, resultCount } = await saveLogs({
+    queue,
+    crawl_id: crawlIds.map(({ current }) => current).join(','),
+    raw_count: null,
+    job_count: jobs.length
+  });
+
+  const msg = `🚀 ${queue.name} init: ${stringifyObjectMsg({
+    num_of_jobs: jobs.length,
+    job_queue_status: jobCounts,
+    pg_raw: null,
+    mongo_updated: resultCount,
+  })}`;
+  console.log(msg);
+  telegramBot.sendMessage(msg);
 };
 
 const scheduleCronJobs = () => {
