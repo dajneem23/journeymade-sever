60b1ebc43f61285ac70f990ac5127f383d85068c
Minh Pham <dev.1foxglobal@gmail.com>
2023-02-21 10:33:40 +0700

    fix: log top holders cron

--- src/core/utils/stringify.ts
+++ src/core/utils/stringify.ts
@@ -1,6 +1,6 @@
 export const stringifyObjectMsg = (obj, pre_text = ' - ') => {
   let msg = ''
-  Object.keys(obj).forEach((key) => {
+  obj && Object.keys(obj).forEach((key) => {
     msg += `\n\t${pre_text}${key}: ${typeof obj[key] === 'object' ? stringifyObjectMsg(obj[key], '\t + ') : obj[key]}`;
   });
 
