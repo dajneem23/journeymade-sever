b06fae7317c14d916bac7ba37e353e910dba65f7
Minh Pham <dev.1foxglobal@gmail.com>
2023-03-01 16:40:02 +0700

    fix: redis password

--- src/configs/ioredis.ts
+++ src/configs/ioredis.ts
@@ -5,12 +5,14 @@ export const ioRedisToken = new Token<IORedis>('_ioRedis');
 
 const port = +process.env.REDIS_PORT;
 const host = process.env.REDIS_HOST;
+const password = process.env.REDIS_PASSWORD;
 
 const ioRedis = async () => {
   try {
     const connection = new IORedis({
       host,
-      port, 
+      port,
+      password, 
       maxRetriesPerRequest: null,
       enableReadyCheck: false,
       lazyConnect: true
